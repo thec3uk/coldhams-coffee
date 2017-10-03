ffbffd6defd621662a4ac07ef0756e3e75c36c96
Andrew Miller <andrew.miller@founders4schools.org.uk>
2017-10-03 13:26:54 +0100

    Initial Commit

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+
+    var feed = new Instafeed({
+        get: 'user',
+        // userId: '2574970955', // Coldhams coffee userId
+        userId: '3413269282', // nanorepublica
+        clientId: '9780059c9bf74c17ab8de4a1c7514dcc',
+        // sortBy: 'most-recent'
+    });
+    feed.run();
